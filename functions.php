f1713db45babcedf499a21ffa55ec1cefad183be
aulladero <carlos@aulladero.com>
2018-11-09 13:50:41 -0600

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,62 @@
+<?php
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+/**
+ * Scripts & Styles.
+ *
+ * Frontend with no conditions, Add Custom styles to wp_head.
+ *
+ * @since  1.0.0
+ */
+function wpgt_scripts() {
+	// Frontend scripts.
+	if ( ! is_admin() ) {
+		// Enqueue vendors first.
+		wp_enqueue_script( 'jquery');
+		// Enqueue custom JS after vendors.
+		wp_enqueue_script( 'wpgt_customJs', get_template_directory_uri() . '/js/custom.min.js' );
+		// Minified and Concatenated styles.
+		wp_enqueue_style( 'wpgt_style', get_template_directory_uri() . '/style.min.css', array(), '1.0', 'all' );
+	}
+}
+// Hook.
+add_action( 'wp_enqueue_scripts', 'wpgt_scripts' );
+
+//Add Featured Image Support
+add_theme_support('post-thumbnails');
+
+// Clean up the <head>
+function removeHeadLinks() {
+	remove_action('wp_head', 'rsd_link');
+	remove_action('wp_head', 'wlwmanifest_link');
+}
+add_action('init', 'removeHeadLinks');
+remove_action('wp_head', 'wp_generator');
+
+function register_menus() {
+	register_nav_menus(
+		array(
+			'main-nav' => 'Main Navigation',
+			'secondary-nav' => 'Secondary Navigation',
+			'sidebar-menu' => 'Sidebar Menu'
+		)
+	);
+}
+add_action( 'init', 'register_menus' );
+
+function register_widgets(){
+
+	register_sidebar( array(
+		'name' => __( 'Sidebar' ),
+		'id' => 'main-sidebar',
+		'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
+		'after_widget' => '</li>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+
+}//end register_widgets()
+add_action( 'widgets_init', 'register_widgets' );
